9b013192bd0c82a5bd2a474610cff7a314e6b042
Dewey Dunnington <dewey@fishandwhistle.net>
2021-03-10 22:35:24 -0400

    organized start to running a model

--- src/session.c
+++ src/session.c
@@ -2,10 +2,13 @@
 #include <R.h>
 #include <Rinternals.h>
 
+#include <stdlib.h>
+
 #include "tensorflow/c/c_api.h"
 
 #include "buffer.h"
 #include "graph.h"
+#include "util.h"
 
 TF_Session* tf_session_from_session_xptr(SEXP session_xptr) {
     return (TF_Session*) R_ExternalPtrAddr(session_xptr);
@@ -92,7 +95,7 @@ SEXP tf_c_load_session_from_saved_model(SEXP export_dir_sexp, SEXP tags_sexp) {
     return session_xptr;
 }
 
-SEXP tf_c_session_xptr_run(SEXP session_xptr, SEXP oper_name_sexp) {
+SEXP tf_c_session_xptr_run(SEXP session_xptr, SEXP oper_name_input_sexp, SEXP oper_name_output_sexp) {
     TF_Session* session = tf_session_from_session_xptr(session_xptr);
     if (session == NULL) {
         Rf_error("TF_Session* is NULL");
@@ -100,20 +103,68 @@ SEXP tf_c_session_xptr_run(SEXP session_xptr, SEXP oper_name_sexp) {
 
     TF_Graph* graph = tf_graph_from_session_xptr(session_xptr);
     if (graph == NULL) {
-        Rf_error("Tf_Graph* is NULL");
+        Rf_error("TF_Graph* is NULL");
     }
 
-    if (TYPEOF(oper_name_sexp) != STRSXP || Rf_length(oper_name_sexp) != 1) {
-        Rf_error("oper_name must be a character vector of length 1");
+    const char* oper_name_input = Rf_translateCharUTF8(STRING_ELT(oper_name_input_sexp, 0));
+    TF_Operation* oper_input = TF_GraphOperationByName(graph, oper_name_input);
+    if (oper_input == NULL) {
+        Rf_error("Can't load input operation '%s' from graph", oper_name_input);
     }
 
-    const char* oper_name = Rf_translateCharUTF8(STRING_ELT(oper_name_sexp, 0));
+    const char* oper_name_output = Rf_translateCharUTF8(STRING_ELT(oper_name_output_sexp, 0));
+    TF_Operation* oper_output = TF_GraphOperationByName(graph, oper_name_output);
+    if (oper_input == NULL) {
+        Rf_error("Can't load output operation '%s' from graph", oper_name_output);
+    }
 
-
+    // inspired by example:
     // https://github.com/AmirulOm/tensorflow_capi_sample
     int num_inputs = 1;
     TF_Output* input = malloc(sizeof(TF_Output) * num_inputs);
+    if (input == NULL) {
+        Rf_error("Failed to alloc input array of size %d", num_inputs);
+    }
     input[0].index = 0;
+    input[0].oper = oper_input;
+
+    int num_outputs = 1;
+    TF_Output* output = malloc(sizeof(TF_Output) * num_outputs);
+    if (input == NULL) {
+        free(input);
+        Rf_error("Failed to alloc output array of size %d", num_outputs);
+    }
+    output[0].index = 0;
+    output[0].oper = oper_output;
+
+    // tensor stuff, should be in its own file with externalptrs
+    TF_Tensor** input_values = malloc(sizeof(TF_Tensor*) * num_inputs);
+    if (input_values == NULL) {
+        free(input);
+        free(output);
+        Rf_error("Failed to alloc input values array of size %d", num_inputs);
+    }
+
+    TF_Tensor** output_values = malloc(sizeof(TF_Tensor*) * num_outputs);
+    if (output_values == NULL) {
+        free(input);
+        free(output);
+        free(input_values);
+        Rf_error("Failed to alloc output values array of size %d", num_inputs);
+    }
+
+    TF_SessionRun(
+        session,
+        // run options can be NULL
+        NULL, 
+        input, input_values, num_inputs, 
+        output, output_values, num_outputs, 
+        // no target operations
+        NULL, 0,
+        // run metadata can be NULL
+        NULL,
+        tf_global_status
+    );
 
     return R_NilValue;
 }
